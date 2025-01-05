53262920cc6a09c089289eff87317fa8ab092adc
Rahul kumar <172767765+Rahulkr3201@users.noreply.github.com>
2025-01-05 16:24:03 +0530

    frontend added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  return (
+    <div className="flex bg-gray-100 min-h-screen">
+      <Sidebar />
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
